ad0f2337c629e667bb89ff4e53fa854886affb4d
Gilbert Fongan <gilbert.fongan@sfmtechnologies.com>
2021-10-28 17:31:01 +0000

    Update centos-lamp.sh

--- Module2-Vagrant/vagrant-lamp/scripts/centos-lamp.sh
+++ Module2-Vagrant/vagrant-lamp/scripts/centos-lamp.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
 
 # Update CentOS with any patches
+echo "Update CentOS with any patches"
 yum update -y --exclude=kernel
 
 # Tools
+echo "Install Tools"
 yum install -y nano git unzip screen wget
 
 # Apache
+echo "Install Apache"
 sudo yum install -y httpd httpd-devel httpd-tools
-sudo chkconfig --add httpd
+
 sudo chkconfig httpd on
 sudo service httpd stop
-
+echo "file modifications"
 sudo rm -rf /var/www/html
 sudo ln -s /vagrant /var/www/html
 
 sudo service httpd start
 
 # PHP
+echo "Install PHP"
 sudo yum install -y php php-cli php-common php-devel php-mysql
 
 # MySQL
+echo "Install MySQL"
 sudo yum install -y mysql mysql-server mysql-devel
 sudo wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
 sudo rpm -ivh mysql-community-release-el7-5.noarch.rpm
 sudo yum update -y
-sudo yum install mysql-server
+sudo yum install -y mysql-server
 sudo service mysqld start
 sudo mysql -e "SHOW DATABASES";
 
 # Download Starter Content
+echo "Install Download content from Gitlab"
 cd /vagrant
 sudo -u vagrant wget -q https://gitlab.com/GilbertFongan/vagrant-lamp/-/raw/master/files/index.html
 sudo -u vagrant wget -q https://gitlab.com/GilbertFongan/vagrant-lamp/-/raw/master/files/info.php
