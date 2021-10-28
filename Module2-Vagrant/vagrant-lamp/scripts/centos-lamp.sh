586417d763841f56f94bef9c3aec9fbe7ae5bf03
Gilbert Fongan <gilbert.fongan@sfmtechnologies.com>
2021-10-28 16:15:05 +0000

    Update centos-lamp.sh

--- Module2-Vagrant/vagrant-lamp/scripts/centos-lamp.sh
+++ Module2-Vagrant/vagrant-lamp/scripts/centos-lamp.sh
@@ -22,12 +22,12 @@ sudo yum install -y php php-cli php-common php-devel php-mysql
 
 # MySQL
 sudo yum install -y mysql mysql-server mysql-devel
-sudo chkconfig --add mysqld
-sudo chkconfig mysqld on
-
+sudo wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
+sudo rpm -ivh mysql-community-release-el7-5.noarch.rpm
+sudo yum update -y
+sudo yum install mysql-server
 sudo service mysqld start
-
-sudo mysql -u root -e "SHOW DATABASES";
+sudo mysql -e "SHOW DATABASES";
 
 # Download Starter Content
 cd /vagrant
