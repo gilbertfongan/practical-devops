e264ae698ac79506c56c19b952e9b8ba3e42e029
GilbertFongan <gilbert.fongan@sfmtechnologies.com>
2023-04-01 01:06:27 +0200

    Update k8s with techiescamp/vagrant-kubeadm-kubernetes

--- Module6-Kubernetes/Kubernetes-Ubuntu-VM/README.md
+++ Module6-Kubernetes/Kubernetes-Ubuntu-VM/README.md
@@ -1,18 +1,39 @@
 
-# Vagrantfile and Scripts to Automate Kubernetes Setup using Kubeadm [Practice Environemnt for CKA/CKAD and CKS Exams]
+# Vagrantfile and Scripts to Automate Kubernetes Setup using Kubeadm [Practice Environment for CKA/CKAD and CKS Exams]
 
 ## Documentation
 
+Current k8s version for CKA, CKAD and CKS exam: 1.26
+
 Refer this link for documentation: https://devopscube.com/kubernetes-cluster-vagrant/
 
-If you are preparing for CKA, CKAD or CKS exam, save $94 using code **DEVOPS25** at https://kube.promo/latest
+## 🚀 CKA, CKAD, CKS or KCNA Coupon Codes
+
+If you are preparing for CKA, CKAD, CKS, or KCNA exam, **save 20%** today using code **SCRIPT20** at https://kube.promo/devops. It is a limited-time offer. Or Check out [Linux Foundation coupon](https://scriptcrunch.com/linux-foundation-coupon/) page for the latest voucher codes.
 
 ## Prerequisites
 
 1. Working Vagrant setup
 2. 8 Gig + RAM workstation as the Vms use 3 vCPUS and 4+ GB RAM
- 
-## Usage/Examples
+
+## For MAC/Linux Users
+
+Latest version of Virtualbox for Mac/Linux can cause issues.
+
+Create/edit the /etc/vbox/networks.conf file and add the following to avoid any network related issues.
+<pre>* 0.0.0.0/0 ::/0</pre>
+
+or run below commands
+
+```shell
+sudo mkdir -p /etc/vbox/
+echo "* 0.0.0.0/0 ::/0" | sudo tee -a /etc/vbox/networks.conf
+```
+
+So that the host only networks can be in any range, not just 192.168.56.0/21 as described here:
+https://discuss.hashicorp.com/t/vagrant-2-2-18-osx-11-6-cannot-create-private-network/30984/23
+
+## Bring Up the Cluster
 
 To provision the cluster, execute the following commands.
 
@@ -21,13 +42,12 @@ git clone https://github.com/scriptcamp/vagrant-kubeadm-kubernetes.git
 cd vagrant-kubeadm-kubernetes
 vagrant up
 ```
-
-## Set Kubeconfig file varaible.
+## Set Kubeconfig file variable
 
 ```shell
 cd vagrant-kubeadm-kubernetes
 cd configs
-export KUBECONFIG=$(PWD)/config
+export KUBECONFIG=$(pwd)/config
 ```
 
 or you can copy the config file to .kube directory.
@@ -36,23 +56,33 @@ or you can copy the config file to .kube directory.
 cp config ~/.kube/
 ```
 
-## Kubernetes Dashboard URL
+## Install Kubernetes Dashboard
 
+The dashboard is automatically installed by default, but it can be skipped by commenting out the dashboard version in _settings.yaml_ before running `vagrant up`.
+
+If you skip the dashboard installation, you can deploy it later by enabling it in _settings.yaml_ and running the following:
+```shell
+vagrant ssh -c "/vagrant/scripts/dashboard.sh" master
+```
+
+## Kubernetes Dashboard Access
+
+To get the login token, copy it from _config/token_ or run the following command:
+```shell
+kubectl -n kubernetes-dashboard get secret/admin-user -o go-template="{{.data.token | base64decode}}"
+```
+
+Proxy the dashboard:
+```shell
+kubectl proxy
+```
+
+Open the site in your browser:
 ```shell
 http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#/overview?namespace=kubernetes-dashboard
 ```
 
-## Kubernetes login token
-
-Vagrant up will create the admin user token and saves in the configs directory.
-
-```shell
-cd vagrant-kubeadm-kubernetes
-cd configs
-cat token
-```
-
-## To shutdown the cluster, 
+## To shutdown the cluster,
 
 ```shell
 vagrant halt
@@ -64,13 +94,9 @@ vagrant halt
 vagrant up
 ```
 
-## To destroy the cluster, 
+## To destroy the cluster,
 
 ```shell
 vagrant destroy -f
 ```
 
-## Centos & HA based Setup
-
-If you want Centos based setup, please refer https://github.com/marthanda93/VAAS
-  
--- Module6-Kubernetes/Kubernetes-Ubuntu-VM/scripts/common.sh
+++ Module6-Kubernetes/Kubernetes-Ubuntu-VM/scripts/common.sh
@@ -1,60 +1,83 @@
-#! /bin/bash
+#!/bin/bash
+#
+# Common setup for all servers (Control Plane and Nodes)
+
+set -euxo pipefail
 
 # Variable Declaration
 
-KUBERNETES_VERSION="1.20.6-00"
-
-# disable swap 
-sudo swapoff -a
-# keeps the swaf off during reboot
-sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
-
-sudo apt-get update -y
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-
-# Following configurations are recomended in the kubenetes documentation for Docker runtime. Please refer https://kubernetes.io/docs/setup/production-environment/container-runtimes/#docker
-
-cat <<EOF | sudo tee /etc/docker/daemon.json
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
-}
+# DNS Setting
+if [ ! -d /etc/systemd/resolved.conf.d ]; then
+	sudo mkdir /etc/systemd/resolved.conf.d/
+fi
+cat <<EOF | sudo tee /etc/systemd/resolved.conf.d/dns_servers.conf
+[Resolve]
+DNS=${DNS_SERVERS}
 EOF
 
-sudo systemctl enable docker
+sudo systemctl restart systemd-resolved
+
+# disable swap
+sudo swapoff -a
+
+# keeps the swap off during reboot
+(crontab -l 2>/dev/null; echo "@reboot /sbin/swapoff -a") | crontab - || true
+sudo apt-get update -y
+# Install CRI-O Runtime
+
+VERSION="$(echo ${KUBERNETES_VERSION} | grep -oE '[0-9]+\.[0-9]+')"
+
+# Create the .conf file to load the modules at bootup
+cat <<EOF | sudo tee /etc/modules-load.d/crio.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Set up required sysctl params, these persist across reboots.
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sudo sysctl --system
+
+cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /
+EOF
+cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list
+deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /
+EOF
+
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
+
+sudo apt-get update
+sudo apt-get install cri-o cri-o-runc -y
+
+cat >> /etc/default/crio << EOF
+${ENVIRONMENT}
+EOF
 sudo systemctl daemon-reload
-sudo systemctl restart docker
+sudo systemctl enable crio --now
 
-echo "Docker Runtime Configured Successfully"
-
+echo "CRI runtime installed susccessfully"
 
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
 sudo apt-get update -y
+sudo apt-get install -y kubelet="$KUBERNETES_VERSION" kubectl="$KUBERNETES_VERSION" kubeadm="$KUBERNETES_VERSION"
+sudo apt-get update -y
+sudo apt-get install -y jq
 
-sudo apt-get install -y kubelet=$KUBERNETES_VERSION kubectl=$KUBERNETES_VERSION kubeadm=$KUBERNETES_VERSION
-
-sudo apt-mark hold kubelet kubeadm kubectl
-
+local_ip="$(ip --json a s | jq -r '.[] | if .ifname == "eth1" then .addr_info[] | if .family == "inet" then .local else empty end else empty end')"
+cat > /etc/default/kubelet << EOF
+KUBELET_EXTRA_ARGS=--node-ip=$local_ip
+${ENVIRONMENT}
+EOF
